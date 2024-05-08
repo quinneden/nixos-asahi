cb3fa3bd510e02e2209c7e3beaecfd06e11e3506
zzywysm <zzy@zzywysm.com>
2024-05-08 10:26:29 -0400

    mesa-asahi: improved config
    prepare for upcoming virtio-gpu support, make more of the Fedora Asahi configuration explicit, disable tests

--- apple-silicon-support/packages/mesa-asahi-edge/default.nix
+++ apple-silicon-support/packages/mesa-asahi-edge/default.nix
@@ -6,23 +6,28 @@
 }:
 
 (mesa.override {
-  galliumDrivers = [ "swrast" "asahi" ];
-  vulkanDrivers = [ "swrast" ];
+  galliumDrivers = [ "asahi" "virgl" "swrast" "kmsro" ];
+  vulkanDrivers = [ "swrast" "virtio" ];
+  vulkanLayers = [ "device-select" ];
+  eglPlatforms = [ "x11" "wayland" ];
+  withLibunwind = false;
+  withValgrind = false;
   enableGalliumNine = false;
+  enablePatentEncumberedCodecs = false;
   # libclc and other OpenCL components are needed for geometry shader support on Apple Silicon
   enableOpenCL = true;
 }).overrideAttrs (oldAttrs: {
   # version must be the same length (i.e. no unstable or date)
   # so that system.replaceRuntimeDependencies can work
   version = "24.1.0";
-  src = fetchFromGitLab {
+  src = lib.cleanSource (fetchFromGitLab {
     # tracking: https://pagure.io/fedora-asahi/mesa/commits/asahi
     domain = "gitlab.freedesktop.org";
     owner = "asahi";
     repo = "mesa";
     rev = "asahi-20240228";
     hash = "sha256-wOFJyYfoN6yxE9HaHXLP/0MhjyRvmlb+jPPUke0sbbE=";
-  };
+  });
 
   mesonFlags =
     # remove flag to configure xvmc functionality as having it
@@ -37,9 +42,44 @@
       # do not want to add the dependencies
       "-Dlibunwind=disabled"
       "-Dlmsensors=disabled"
-    ] ++ ( # does not compile on nixpkgs stable, doesn't seem mandatory
-      lib.optional (lib.versionOlder meson.version "1.3.1")
-        "-Dgallium-rusticl=false");
+      "-Dgallium-omx=disabled"
+      "-Dgallium-d3d12-video=disabled"
+      "-Dxlib-lease=disabled"
+      # add options from Fedora Asahi's meson flags we're missing
+      # some of these get picked up automatically since
+      # auto-features is enabled, but better to be explicit
+      # in the same places as Fedora is explicit
+      "-Dgallium-opencl=icd"
+      "-Dgallium-rusticl=true"
+      "-Ddri3=enabled"
+      "-Degl=enabled"
+      "-Dgbm=enabled"
+      "-Dopengl=true"
+      "-Dshared-glapi=enabled"
+      "-Dgles1=enabled"
+      "-Dgles2=enabled"
+      "-Dglx=dri"
+      "-Dglvnd=true"
+      # enable LLVM specifically (though auto-features seems to turn it on)
+      # and enable shared-LLVM specifically like Fedora Asahi does
+      # perhaps the lack of shared-llvm was/is breaking rusticl? needs investigation
+      "-Dllvm=enabled"
+      "-Dshared-llvm=enabled"
+      # add in additional options from mesa-asahi's meson options,
+      # mostly to make explicit what was once implicit (the Nix way!)
+      "-Degl-native-platform=wayland"
+      "-Dandroid-strict=false"
+      "-Dpower8=disabled"
+      "-Dvideo-codecs=vp9dec"
+      # save time, don't build tests
+      "-Dbuild-tests=false"
+      "-Denable-glcpp-tests=false"
+      # the upstream nixpkgs code for detecting whether we want intel-clc
+      # is deeply flawed and is likely causing subtle build differences
+      # between cross-compiles and non-cross-compiles.
+      # disable it using the same lib.mesonEnable-type call that nixpkgs uses
+      (lib.mesonEnable "intel-clc" false)
+    ];
 
   # replace patches with ones tweaked slightly to apply to this version
   patches = [
